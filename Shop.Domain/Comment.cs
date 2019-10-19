d5aaf3bf64ce21c53c3e90a2bf0cbac842a5b0a9
arkonq@gmail.com <arkonq@gmail.com>
2019-10-19 16:02:57 +0600

    Добавьте файлы проекта.

---
+++ Shop.Domain/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Domain
+{
+	public class Comment : Entity		
+	{
+		public Guid UserId { get; set; }
+		public Guid ItemId { get; set; }
+		public string Value { get; set; }
+	}
+}
